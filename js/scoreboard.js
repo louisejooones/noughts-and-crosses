7e7a0671a698a677b1b254924599c0fc6938dbce
Louise Jones <louisejones94@live.co.uk>
2023-03-29 16:54:29 +0100

    Scoreboard class

---
+++ js/scoreboard.js
@@ -0,0 +1,9 @@
+class Scoreboard {
+    constructor() {
+        this.firstPlayer = 0;
+        this.secondPlayer = 0;
+        this.currentPlayer = this.firstPlayer;
+    }
+}
+
+export { Scoreboard };
